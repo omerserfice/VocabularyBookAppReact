70906fde2a3c1f7e8be2f5d9fcc40dd5177ddc97
Ömer Serfice <srfcomr@gmail.com>
2025-02-24 01:08:36 +0300

    router eklendi

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+const Navbar = () => {
+  return (
+
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <div className="container-fluid">
+      <a className="navbar-brand" href="#">Vocabulary Book App</a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+             <Link className="nav-link active" to="/">Anasayfa</Link>
+          </li>
+          <li className="nav-item">
+          <Link className="nav-link active" to="/about">Hakkımızda</Link>
+          </li>
+         
+          <li className="nav-item">
+          <Link className="nav-link active" to="/contact">İletişim</Link>
+          </li>
+        </ul>
+        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Detay
+            </a>
+            <ul className="dropdown-menu" style={{ marginLeft:'-50px'}}>
+              <li><a className="dropdown-item" href="#">Profil</a></li>
+              <li><a className="dropdown-item" href="#">Çıkış Yap</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  );
+};
+
+export default Navbar;
